2aa4de351803daca257265f81f29b51f8c16e798
WMF Industries <87574232+WMF-Industries@users.noreply.github.com>
2022-05-28 07:59:13 +0200

    Add files via upload

---
+++ scripts/graphics/pal.js
@@ -0,0 +1,6 @@
+Events.on(ClientLoadEvent, b  => {
+Pal.heal = Color.valueOf("6cf5d7");
+Pal.sap = Color.valueOf("814c9e");
+Pal.sapBullet = Color.valueOf("f17afa");
+Pal.sapBulletBack = Color.valueOf("7343bf");
+});
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+require("graphics/pal")
+require("units/NeonSupports")
+require("units/NeonSpecialists")
